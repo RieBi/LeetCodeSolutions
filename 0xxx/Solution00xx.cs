7b93e3b6089b0ec32b415408699449afc2e360ea
RieBi <42877671+RieBi@users.noreply.github.com>
2023-11-19 09:52:48 +0200

    Solved problem 9

---
+++ 0xxx/Solution00xx.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Set0xxx;
+internal class Solution00xx
+{
+    #region Solution for 9
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+        if (x < 10)
+            return true;
+        var log = (int)(Math.Log10(x) + 1);
+        var ar = new int[log];
+
+        for (int i = 0; i < ar.Length; i++)
+        {
+            ar[i] = x % 10;
+            x /= 10;
+        }
+
+        for (int i = 0; i < ar.Length / 2; i++)
+        {
+            if (ar[i] != ar[ar.Length - 1 - i])
+                return false;
+        }
+
+        return true;
+    }
+    #endregion
+}
