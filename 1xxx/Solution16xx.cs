a58c9b1172022ba7b3fdd8d6b49391fa1bef6e1c
RieBi <42877671+RieBi@users.noreply.github.com>
2023-11-25 23:03:19 +0200

    Added solution for 1685 problem from daily challenge

--- 1xxx/Solution16xx.cs
+++ 1xxx/Solution16xx.cs
@@ -29,4 +29,37 @@ internal class Solution16xx
         return result;
     }
     #endregion
+
+    #region Solution for 1685
+    public int[] GetSumAbsoluteDifferences(int[] nums)
+    {
+        var prefSums = new int[nums.Length];
+        prefSums[0] = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            prefSums[i] = prefSums[i - 1] + nums[i];
+        }
+
+        int getSum(int l, int r)
+        {
+            if (l == 0)
+                return prefSums[r];
+
+            return prefSums[r] - prefSums[l - 1];
+        }
+
+        var resultArr = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var sum = 0;
+            if (i != nums.Length - 1)
+                sum += getSum(i + 1, nums.Length - 1) - nums[i] * (nums.Length - 1 - i);
+            if (i != 0)
+                sum += nums[i] * i - getSum(0, i - 1);
+            resultArr[i] = sum;
+        }
+
+        return resultArr;
+    }
+    #endregion
 }
