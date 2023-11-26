68127b13fa7306c3a9a5d810060799751a8a9de7
RieBi <42877671+RieBi@users.noreply.github.com>
2023-11-26 20:43:06 +0200

    Added a solution for 1727 problem from daily challenge

--- 1xxx/Solution17xx.cs
+++ 1xxx/Solution17xx.cs
@@ -1,6 +1,37 @@
 ﻿namespace LeetCode.Set1xxx;
 internal class Solution17xx
 {
+    #region Solution for 1727
+    public int LargestSubmatrix(int[][] matrix)
+    {
+        var m = matrix.Length;
+        var n = matrix[0].Length;
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] != 0)
+                    matrix[i][j] = matrix[i - 1][j] + 1;
+            }
+        }
+
+        var max = 0;
+        for (int i = 0; i < m; i++)
+        {
+            var row = matrix[i];
+            Array.Sort(row);
+            for (int j = n; j > 0; j--)
+            {
+                var localMax = j * row[n - j];
+                max = Math.Max(max, localMax);
+            }
+        }
+
+        return max;
+    }
+    #endregion
+
     #region Solution for 1759
     public int CountHomogenous(string s)
     {
