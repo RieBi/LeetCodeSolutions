959c1103f23f7ec733bea4bbd99b2941eb56bb00
RieBi <42877671+RieBi@users.noreply.github.com>
2023-11-28 21:37:59 +0200

    Added solution for 2147 from daily challenge

---
+++ 2xxx/Solution21xx.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Set2xxx;
+internal class Solution21xx
+{
+    #region Solution for 2147
+    public int NumberOfWays(string corridor)
+    {
+        var result = 1L;
+        var curStreak = 0;
+        var plantStreak = 0;
+        var modulo = 1_000_000_007l;
+        for (int i = 0; i < corridor.Length; i++)
+        {
+            var curObject = corridor[i];
+            if (curStreak != 2)
+            {
+                if (curObject == 'S')
+                    curStreak++;
+            }
+            else
+            {
+                if (curObject == 'S')
+                {
+                    result *= (long)(plantStreak + 1);
+                    result %= modulo;
+                    curStreak = 1;
+                    plantStreak = 0;
+                }
+                else if (curObject == 'P')
+                {
+                    plantStreak++;
+                }
+            }
+        }
+
+        if (curStreak != 2)
+            result = 0;
+
+        return (int)result;
+    }
+    #endregion
+}
