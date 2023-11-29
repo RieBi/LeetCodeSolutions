4ef01a4c9f8ed06d3a4c82515ac51fea762134a4
RieBi <42877671+RieBi@users.noreply.github.com>
2023-11-29 17:34:04 +0200

    Added a solution for 191 problem from daily challenge

---
+++ 0xxx/Solution01xx.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Set0xxx;
+internal class Solution01xx
+{
+    #region Solution for 191
+    public int HammingWeight(uint n)
+    {
+        var result = 0;
+        uint mask = 1;
+        for (int i = 0; i < 32; i++)
+        {
+            if ((n & mask) == mask)
+                result++;
+            mask <<= 1;
+        }
+
+        return result;
+    }
+    #endregion
+}
