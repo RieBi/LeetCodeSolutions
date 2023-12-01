fecd093a82d80653df4a6f46cfd8c99d17a11f77
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-01 18:20:04 +0200

    Added a solution for problem 2869

--- 2xxx/Solution28xx.cs
+++ 2xxx/Solution28xx.cs
@@ -16,4 +16,28 @@ internal class Solution28xx
         return t >= time;
     }
     #endregion
+
+    #region Solution for 2869
+    public int MinOperations(IList<int> nums, int k)
+    {
+        var doneInts = new bool[k + 1];
+        var doneIntsCount = 0;
+        var ops = 0;
+
+        for (int i = nums.Count - 1; i >= 0; i--)
+        {
+            if (nums[i] <= k && doneInts[nums[i]] == false)
+            {
+                doneIntsCount++;
+                doneInts[nums[i]] = true;
+            }
+
+            ops++;
+            if (doneIntsCount == k)
+                return ops;
+        }
+
+        return -1;
+    }
+    #endregion
 }
