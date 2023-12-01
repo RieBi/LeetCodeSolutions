2ac917ea3128270954b1254de89f74baaf3182ad
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-01 18:43:59 +0200

    Added a custom attribute and method for finding members with it

--- Program.cs
+++ Program.cs
@@ -1,16 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
-namespace LeetCode
+namespace LeetCode;
+
+internal class Program
 {
-    internal class Program
+    public static void Main()
     {
-        public static void Main()
+        var count = 0;
+    }
+
+    public static void ForEachTypeContainingAttribute<T>(Action<T> action) where T : System.Attribute
+    {
+        var assembly = Assembly.GetCallingAssembly();
+        var types = assembly.GetTypes();
+        foreach (var type in types)
         {
-            Console.WriteLine("Hello, LeetCode!");
+            var methods = type.GetMethods();
+            foreach (var method in methods)
+            {
+                var attribute = method.GetCustomAttribute<T>();
+                if (attribute is not null)
+                    action(attribute);
+            }
+
+            var typeAttribute = type.GetCustomAttribute<T>();
+            if (typeAttribute is not null)
+                action(typeAttribute);
         }
+    }
+}
+
+internal class ProblemSolutionAttribute
+{
+    public string? ProblemName { get; set; }
+
+    public ProblemSolutionAttribute(string? problemName = null)
+    {
+        ProblemName = problemName;
     }
 }
