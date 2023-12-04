18fa04998191c418b46fa5eae47494003a7c60b5
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-04 20:05:51 +0200

    Added solution for problem 2264 (from daily challenge)

--- 2xxx/Solution22xx.cs
+++ 2xxx/Solution22xx.cs
@@ -1,4 +1,23 @@
-﻿namespace LeetCode.Set2xxx;
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode.Set2xxx;
 internal class Solution22xx
 {
+    [ProblemSolution("2264")]
+    public string LargestGoodInteger(string num)
+    {
+        var pattern = @"(\d)\1{2}";
+        var matches = Regex.Matches(num, pattern);
+        if (matches.Count == 0)
+            return "";
+
+        var max = "000";
+        foreach (Match match in matches)
+        {
+            if (match.Value.CompareTo(max) > 0)
+                max = match.Value;
+        }
+
+        return max;
+    }
 }
