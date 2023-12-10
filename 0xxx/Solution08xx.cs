c1a1c5265a3ebfd83eae603eef89a3a33e94d2d9
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-10 10:08:05 +0200

    Added solution for problem 867 (from daily challenge)

--- 0xxx/Solution08xx.cs
+++ 0xxx/Solution08xx.cs
@@ -7,4 +7,24 @@ using System.Threading.Tasks;
 namespace LeetCode._0xxx;
 internal class Solution08xx
 {
+    [ProblemSolution("867")]
+    public int[][] Transpose(int[][] matrix)
+    {
+        var rows = matrix.Length;
+        var cols = matrix[0].Length;
+
+        var resMatrix = new int[cols][];
+        for (int i = 0; i < cols; i++)
+            resMatrix[i] = new int[rows];
+
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                resMatrix[j][i] = matrix[i][j];
+            }
+        }
+
+        return resMatrix;
+    }
 }
