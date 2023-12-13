695da71e0ad9dd7e55e97b3a8d1ba2c4c88d6ee1
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-13 14:11:01 +0200

    Added solution to problem 1582 (from daily challenge)

--- 1xxx/Solution15xx.cs
+++ 1xxx/Solution15xx.cs
@@ -48,4 +48,36 @@ internal class Soltuion15xx
 
         return result;
     }
+
+    public int NumSpecial(int[][] mat)
+    {
+        var rows = mat
+            .Select(f => f.Count(el => el == 1))
+            .ToList();
+
+        var cols = new List<int>(mat[0].Length);
+        for (int i = 0; i < mat[0].Length; i++)
+        {
+            var count = 0;
+            for (int j = 0; j < mat.Length; j++)
+            {
+                if (mat[j][i] == 1)
+                    count++;
+            }
+
+            cols.Add(count);
+        }
+
+        var total = 0;
+        for (int i = 0; i < mat.Length; i++)
+        {
+            for (int j = 0; j < mat[0].Length; j++)
+            {
+                if (mat[i][j] == 1 && rows[i] == 1 && cols[j] == 1)
+                    total++;
+            }
+        }
+
+        return total;
+    }
 }
