c39cc6c28b05b8fdf04655c85fea3440a48fd892
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-16 13:26:34 +0200

    Added solution to problem 242 (from daily challenge)

--- 0xxx/Solution02xx.cs
+++ 0xxx/Solution02xx.cs
@@ -1,4 +1,42 @@
 ﻿namespace LeetCode.Set0xxx;
 internal class Solution02xx
 {
+    [ProblemSolution("242")]
+    public bool IsAnagram(string s, string t)
+    {
+        var dicks = new Dictionary<char, int>();
+        var dickt = new Dictionary<char, int>();
+
+        FillDick(s, dicks);
+        FillDick(t, dickt);
+
+        return DicksEqual(dicks, dickt);
+
+        void FillDick(string str, Dictionary<char, int> dick)
+        {
+            foreach (var v in str)
+            {
+                if (dick.TryGetValue(v, out int val))
+                    dick[v] = val + 1;
+                else
+                    dick[v] = 1;
+            }
+        }
+
+        bool DicksEqual(Dictionary<char, int> dick1, Dictionary<char, int> dick2)
+        {
+            if (dick1.Count != dick2.Count)
+                return false;
+
+            foreach (var v in dick1.Keys)
+            {
+                if (!(dick1.TryGetValue(v, out int val1) && dick2.TryGetValue(v, out int val2)))
+                    return false;
+                else if (val1 != val2)
+                    return false;
+            }
+
+            return true;
+        }
+    }
 }
