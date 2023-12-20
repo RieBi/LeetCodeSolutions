70b7c3e1d48ca2c93489be2b6823d95020bf0cb2
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-20 19:40:52 +0200

    Added solution to problem 1020

--- 1xxx/Solution10xx.cs
+++ 1xxx/Solution10xx.cs
@@ -1,4 +1,36 @@
 ﻿namespace LeetCode.Set1xxx;
 internal class Solution10xx
 {
+    [ProblemSolution("1020")]
+    public int NumEnclaves(int[][] grid)
+    {
+        for (int i = 0; i < grid[0].Length; i++)
+        {
+            processCell(0, i);
+            processCell(grid.Length - 1, i);
+        }
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+            processCell(i, 0);
+            processCell(i, grid[0].Length - 1);
+        }
+
+        return grid.Sum(f => f.Count(x => x == 1));
+
+        void processCell(int i, int j)
+        {
+            if (i < 0 || j < 0 || i >= grid.Length || j >= grid[0].Length)
+                return;
+            if (grid[i][j] != 1)
+                return;
+            
+            grid[i][j] = -1;
+
+            processCell(i - 1, j);
+            processCell(i + 1, j);
+            processCell(i, j - 1);
+            processCell(i, j + 1);
+        }
+    }
 }
