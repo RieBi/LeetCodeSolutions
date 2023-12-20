1d6644e4ec4e309440b33e0eaee09385eeb25400
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-20 19:14:33 +0200

    Added solution to problem 2706 (from daily challenge)

--- 2xxx/Solution27xx.cs
+++ 2xxx/Solution27xx.cs
@@ -1,4 +1,24 @@
 ﻿namespace LeetCode.Set2xxx;
 internal class Solution27xx
 {
+    [ProblemSolution("2706")]
+    public int BuyChoco(int[] prices, int money)
+    {
+        var min1 = prices[0];
+        var min2 = prices[1];
+        if (min1 > min2)
+            (min1, min2) = (min2, min1);
+
+        for (int i = 2; i < prices.Length; i++)
+        {
+            if (prices[i] < min2)
+                min2 = prices[i];
+
+            if (min1 > min2)
+                (min1, min2) = (min2, min1);
+        }
+
+        var leftover = money - min1 - min2;
+        return leftover >= 0 ? leftover : money;
+    }
 }
