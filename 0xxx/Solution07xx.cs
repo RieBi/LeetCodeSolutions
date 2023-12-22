c329d89f2f16544f181250a069739712bb1d4d81
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-22 19:43:49 +0200

    Added solution to problem 704

--- 0xxx/Solution07xx.cs
+++ 0xxx/Solution07xx.cs
@@ -7,4 +7,23 @@ using System.Threading.Tasks;
 namespace LeetCode._0xxx;
 internal class Solution07xx
 {
+    [ProblemSolution("704")]
+    public int Search(int[] nums, int target)
+    {
+        var low = 0;
+        var high = nums.Length - 1;
+        while (low <= high)
+        {
+            var mid = (high + low) / 2;
+            var num = nums[mid];
+            if (num == target)
+                return mid;
+            else if (num > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return -1;
+    }
 }
