5aab30925f69a0af8aa7dd8de15e3175f53c3cff
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-24 10:06:37 +0200

    Added solution to problem 599

--- 0xxx/Solution05xx.cs
+++ 0xxx/Solution05xx.cs
@@ -1,4 +1,17 @@
 ﻿namespace LeetCode.Set0xxx;
 internal class Solution05xx
 {
+    [ProblemSolution("599")]
+    public string[] FindRestaurant(string[] list1, string[] list2)
+    {
+        var commons = list1.Zip(Enumerable.Range(0, list1.Length))
+            .Concat(list2.Zip(Enumerable.Range(0, list2.Length)))
+            .GroupBy(f => f.First)
+            .Where(f => f.Count() == 2);
+
+        var min = commons.Select(f => f.First().Second + f.Last().Second).Min();
+        return commons.Where(f => f.First().Second + f.Last().Second == min)
+            .Select(f => f.Key)
+            .ToArray();
+    }
 }
