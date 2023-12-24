c03e0a76ad1d0e98e730063d1d72dfa5c6d2074e
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-24 10:25:22 +0200

    Added solution to problem 387

--- 0xxx/Solution03xx.cs
+++ 0xxx/Solution03xx.cs
@@ -3,4 +3,14 @@ internal class Solution03xx
 {
     [ProblemSolution("349")]
     public int[] Intersection(int[] nums1, int[] nums2) => nums1.Intersect(nums2).ToArray();
+
+    [ProblemSolution("387")]
+    public int FirstUniqChar(string s)
+    {
+        return s.Zip(Enumerable.Range(0, s.Length))
+            .GroupBy(f => f.First, s => s.Second)
+            .Where(f => f.Count() == 1)
+            .Select(f => f.Single())
+            .FirstOrDefault(-1);
+    }
 }
