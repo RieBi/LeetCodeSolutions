3a691f94579ed2c149c6e6c05c2da61950b86d2e
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-24 10:35:23 +0200

    Added solution to problem 350

--- 0xxx/Solution03xx.cs
+++ 0xxx/Solution03xx.cs
@@ -4,6 +4,25 @@ internal class Solution03xx
     [ProblemSolution("349")]
     public int[] Intersection(int[] nums1, int[] nums2) => nums1.Intersect(nums2).ToArray();
 
+    [ProblemSolution("350")]
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        var group1 = nums1.GroupBy(f => f).Select(f => (f.Key, f.Count())).ToDictionary();
+        var group2 = nums2.GroupBy(f => f).Select(f => (f.Key, f.Count())).ToDictionary();
+
+        var result = new List<int>();
+        foreach (var group in group1)
+        {
+            if (!group2.TryGetValue(group.Key, out int value))
+                continue;
+            var count = Math.Min(group.Value, value);
+            for (int i = 0; i < count; i++)
+                result.Add(group.Key);
+        }
+
+        return result.ToArray();
+    }
+
     [ProblemSolution("387")]
     public int FirstUniqChar(string s)
     {
