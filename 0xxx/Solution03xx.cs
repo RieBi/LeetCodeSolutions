db31bd985c2f365919711080ddb5b044a8970ea7
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-24 09:17:18 +0200

    Added solution to problem 349

--- 0xxx/Solution03xx.cs
+++ 0xxx/Solution03xx.cs
@@ -1,4 +1,6 @@
 ﻿namespace LeetCode.Set0xxx;
 internal class Solution03xx
 {
+    [ProblemSolution("349")]
+    public int[] Intersection(int[] nums1, int[] nums2) => nums1.Intersect(nums2).ToArray();
 }
