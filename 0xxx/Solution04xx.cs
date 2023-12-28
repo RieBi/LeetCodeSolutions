84a570b2a7eed3c3bcb7a4b559865d11861ebb76
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-28 22:50:13 +0200

    Added solution to problem 454

--- 0xxx/Solution04xx.cs
+++ 0xxx/Solution04xx.cs
@@ -37,4 +37,45 @@ internal class Solution04xx
 
         return pos;
     }
+
+    [ProblemSolution("454")]
+    public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4)
+    {
+        var n = nums1.Length;
+        var dick1 = new Dictionary<int, int>();
+        var dick2 = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                var sum = nums1[i] + nums2[j];
+                if (dick1.TryGetValue(sum, out int value))
+                    dick1[sum] = value + 1;
+                else
+                    dick1[sum] = 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                var sum = nums3[i] + nums4[j];
+                if (dick2.TryGetValue(sum, out int value))
+                    dick2[sum] = value + 1;
+                else
+                    dick2[sum] = 1;
+            }
+        }
+
+        var result = 0;
+        foreach (var v in dick1)
+        {
+            if (dick2.TryGetValue(-v.Key, out int value))
+                result += v.Value * value;
+        }
+
+        return result;
+    }
 }
