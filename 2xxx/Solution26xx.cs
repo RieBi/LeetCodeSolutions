69da62a27a0de68c73bc10f7e2a44705784de46a
RieBi <42877671+RieBi@users.noreply.github.com>
2024-01-02 21:49:01 +0200

    Added solution to problem 2610 (from daily challenge)

--- 2xxx/Solution26xx.cs
+++ 2xxx/Solution26xx.cs
@@ -1,4 +1,30 @@
 ﻿namespace LeetCode.Set2xxx;
 internal class Solution26xx
 {
+    [ProblemSolution("2610")]
+    public IList<IList<int>> FindMatrix(int[] nums)
+    {
+        var dick = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            if (dick.TryGetValue(num, out int count))
+                dick[num] = count + 1;
+            else
+                dick[num] = 1;
+        }
+
+        var result = new List<IList<int>>();
+        foreach (var v in dick)
+        {
+            for (int i = 0; i < v.Value; i++)
+            {
+                if (result.Count == i)
+                    result.Add([]);
+
+                result[i].Add(v.Key);
+            }
+        }
+
+        return result;
+    }
 }
