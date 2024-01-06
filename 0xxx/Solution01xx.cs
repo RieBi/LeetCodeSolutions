f98daaab5bdadcfa8134798a78ad790a130a6653
RieBi <42877671+RieBi@users.noreply.github.com>
2024-01-06 17:10:26 +0200

    Added solution to problem 155

--- 0xxx/Solution01xx.cs
+++ 0xxx/Solution01xx.cs
@@ -7,6 +7,20 @@ internal class Solution01xx
         return nums.Aggregate((a, b) => a^b);
     }
 
+    [ProblemSolution("155")]
+    public class MinStack
+    {
+        private readonly List<(int val, int min)> list = [];
+
+        public void Push(int val) => list.Add((val, list.Count == 0 ? val : Math.Min(val, list[^1].min)));
+
+        public void Pop() => list.RemoveAt(list.Count - 1);
+
+        public int Top() => list[^1].val;
+
+        public int GetMin() => list[^1].min;
+    }
+
     [ProblemSolution("191")]
     public int HammingWeight(uint n)
     {
