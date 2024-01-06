0831ee4f7a98d5c17d54ead2734b7c01453f2e84
RieBi <42877671+RieBi@users.noreply.github.com>
2024-01-06 23:27:44 +0200

    Added solution to problem 841

--- 0xxx/Solution08xx.cs
+++ 0xxx/Solution08xx.cs
@@ -7,6 +7,30 @@ using System.Threading.Tasks;
 namespace LeetCode._0xxx;
 internal class Solution08xx
 {
+    [ProblemSolution("841")]
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        var keys = new HashSet<int>();
+        keys.Add(0);
+        var queue = new Queue<int>();
+        queue.Enqueue(0);
+
+        while (queue.Count > 0)
+        {
+            var room = queue.Dequeue();
+            foreach (var key in rooms[room])
+            {
+                if (!keys.Contains(key))
+                {
+                    keys.Add(key);
+                    queue.Enqueue(key);
+                }
+            }
+        }
+
+        return keys.Count == rooms.Count;
+    }
+
     [ProblemSolution("867")]
     public int[][] Transpose(int[][] matrix)
     {
