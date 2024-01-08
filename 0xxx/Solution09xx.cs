e43cf52d5a654caea2203571a08dce699c122bc3
RieBi <42877671+RieBi@users.noreply.github.com>
2024-01-08 11:49:45 +0200

    Added solution to problem 938

--- 0xxx/Solution09xx.cs
+++ 0xxx/Solution09xx.cs
@@ -7,4 +7,24 @@ using System.Threading.Tasks;
 namespace LeetCode._0xxx;
 internal class Solution09xx
 {
+    [ProblemSolution("938")]
+    public int RangeSumBST(TreeNode root, int low, int high)
+    {
+        var sum = 0;
+        propagate(root);
+
+        void propagate(TreeNode? node)
+        {
+            if (node == null)
+                return;
+
+            if (node.val >= low && node.val <= high)
+                sum += node.val;
+
+            propagate(node.left);
+            propagate(node.right);
+        }
+
+        return sum;
+    }
 }
