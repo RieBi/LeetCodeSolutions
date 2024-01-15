fe776b30ecad84acc44340f3debe16843d6cd8d9
RieBi <42877671+RieBi@users.noreply.github.com>
2024-01-15 16:29:43 +0200

    Added solution to problem 2225 (from daily challenge)

--- 2xxx/Solution22xx.cs
+++ 2xxx/Solution22xx.cs
@@ -3,6 +3,36 @@
 namespace LeetCode.Set2xxx;
 internal class Solution22xx
 {
+    [ProblemSolution("2225")]
+    public IList<IList<int>> FindWinners(int[][] matches)
+    {
+        var dick = new SortedDictionary<int, int>();
+        foreach (var match in matches)
+        {
+            if (!dick.ContainsKey(match[0]))
+                dick[match[0]] = 0;
+
+            if (dick.TryGetValue(match[1], out int value))
+                dick[match[1]] = value + 1;
+            else
+                dick[match[1]] = 1;
+        }
+
+        var result = new List<IList<int>>();
+        result.Add([]);
+        result.Add([]);
+
+        foreach (var pair in dick)
+        {
+            if (pair.Value == 0)
+                result[0].Add(pair.Key);
+            else if (pair.Value == 1)
+                result[1].Add(pair.Key);
+        }
+
+        return result;
+    }
+
     [ProblemSolution("2264")]
     public string LargestGoodInteger(string num)
     {
