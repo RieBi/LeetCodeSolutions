cd3bc0ef4f51cd727360be3d8612e2299550770e
RieBi <42877671+RieBi@users.noreply.github.com>
2024-02-18 16:15:49 +0200

    Added solution to problem 2402

--- 2xxx/Solution24xx.cs
+++ 2xxx/Solution24xx.cs
@@ -1,6 +1,42 @@
 ﻿namespace LeetCode.Set2xxx;
 internal class Solution24xx
 {
+    [ProblemSolution("2402")]
+    public int MostBooked(int n, int[][] meetings)
+    {
+        Array.Sort(meetings, (a, b) => a[0].CompareTo(b[0]));
+        var vacantRooms = new PriorityQueue<int, int>();
+        for (int i = 0; i < n; i++)
+            vacantRooms.Enqueue(i, i);
+
+        var rooms = new int[n];
+        var meets = new PriorityQueue<(long end, int room), (long end, int room)>();
+        long t = 0;
+
+        for (int i = 0; i < meetings.Length; i++)
+        {
+            var meeting = meetings[i];
+            var start = meeting[0];
+            var end = meeting[1];
+            t = Math.Max(start, t);
+
+            while (meets.Count > 0 && t >= meets.Peek().end || vacantRooms.Count == 0)
+            {
+                var last = meets.Dequeue();
+                vacantRooms.Enqueue(last.room, last.room);
+                t = Math.Max(t, last.end);
+            }
+
+            var room = vacantRooms.Dequeue();
+            rooms[room]++;
+            var realEnd = t + (end - start);
+            meets.Enqueue((realEnd, room), (realEnd, room));
+        }
+
+        var max = rooms.Max();
+        return rooms.ToList().IndexOf(max);
+    }
+
     [ProblemSolution("2482")]
     public int[][] OnesMinusZeros(int[][] grid)
     {
