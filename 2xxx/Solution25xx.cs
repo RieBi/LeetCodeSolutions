248e6fdef65b6273693b8177466a29e265705d6f
RieBi <42877671+RieBi@users.noreply.github.com>
2024-03-09 16:16:37 +0200

    Added solution to problem 2540 (daily challenge)

--- 2xxx/Solution25xx.cs
+++ 2xxx/Solution25xx.cs
@@ -1,4 +1,21 @@
 ﻿namespace LeetCode.Set2xxx;
 internal class Solution25xx
 {
+    [ProblemSolution("2540")]
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        var p1 = 0;
+        var p2 = 0;
+        while (p1 < nums1.Length && p2 < nums2.Length)
+        {
+            if (nums1[p1] == nums2[p2])
+                return nums1[p1];
+            else if (nums1[p1] < nums2[p2])
+                p1++;
+            else
+                p2++;
+        }
+
+        return -1;
+    }
 }
