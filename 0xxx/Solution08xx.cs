a90d1c9be5d30b89b521c3e0cf09ef9e49fabe94
RieBi <riebisv@gmail.com>
2024-04-28 13:03:40 +0300

    Added solution to problem 834 (daily challenge)

--- 0xxx/Solution08xx.cs
+++ 0xxx/Solution08xx.cs
@@ -1,12 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LeetCode._0xxx;
 internal class Solution08xx
 {
+    [ProblemSolution("834")]
+    public int[] SumOfDistancesInTree(int n, int[][] edges)
+    {
+        if (n == 1)
+            return [0];
+
+        var nodes = new List<int>[n];
+        var parents = new int[n];
+        var total = new int[n];
+        var childCounts = new int[n];
+
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            nodes[i] = [];
+            parents[i] = -1;
+            total[i] = -1;
+            childCounts[i] = -1;
+        }
+
+        for (int i = 0; i < edges.Length; i++)
+        {
+            var nodea = edges[i][0];
+            var nodeb = edges[i][1];
+            nodes[nodea].Add(nodeb);
+            nodes[nodeb].Add(nodea);
+        }
+
+        var queue = new Queue<int>();
+        queue.Enqueue(0);
+        while (queue.Count > 0)
+        {
+            var last = queue.Dequeue();
+            foreach (int other in nodes[last])
+            {
+                if (other == parents[last])
+                    continue;
+
+                parents[other] = last;
+                queue.Enqueue(other);
+            }
+        }
+
+
+        var stack = new Stack<int>();
+        stack.Push(0);
+        while (stack.Count > 0)
+        {
+            var top = stack.Peek();
+
+            if (nodes[top].Count == 1 && parents[top] != -1)
+                childCounts[top] = 0;
+            if (childCounts[top] == -1)
+            {
+                foreach (var other in nodes[top])
+                {
+                    if (other == parents[top])
+                        continue;
+
+                    stack.Push(other);
+                }
+            }
+            else
+            {
+                if (parents[top] != -1)
+                {
+                    if (childCounts[parents[top]] == -1)
+                        childCounts[parents[top]] = 0;
+                    childCounts[parents[top]] += 1 + childCounts[top];
+                }
+
+                stack.Pop();
+            }
+        }
+
+        var startTotal = 0;
+        var level = 0;
+        queue.Enqueue(0);
+        while (queue.Count > 0)
+        {
+            var c = queue.Count;
+            for (int i = 0; i < c; i++)
+            {
+                var last = queue.Dequeue();
+                startTotal += level;
+                foreach (var other in nodes[last])
+                {
+                    if (other == parents[last])
+                        continue;
+
+                    queue.Enqueue(other);
+                }
+            }
+
+            level++;
+        }
+
+        total[0] = startTotal;
+        queue.Enqueue(0);
+        while (queue.Count > 0)
+        {
+            var c = queue.Count;
+            for (int i = 0; i < c; i++)
+            {
+                var last = queue.Dequeue();
+                foreach (var other in nodes[last])
+                {
+                    if (other == parents[last])
+                        continue;
+
+                    queue.Enqueue(other);
+                }
+
+                if (parents[last] != -1)
+                {
+                    total[last] = total[parents[last]] - childCounts[last] * 2 + n - 2;
+                }
+            }
+        }
+
+        return total;
+    }
+
     [ProblemSolution("841")]
     public bool CanVisitAllRooms(IList<IList<int>> rooms)
     {
