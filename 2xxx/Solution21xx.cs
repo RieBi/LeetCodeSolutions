075ac8ac5e17b7e7702de2e13f6e32680a4df785
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-15 13:30:52 +0200

    Add solution to problem 2196: Create Binary Tree From Descriptions (daily challenge)

--- 2xxx/Solution21xx.cs
+++ 2xxx/Solution21xx.cs
@@ -177,4 +177,39 @@ internal class Solution21XX
 
         return result;
     }
+
+    [ProblemSolution("2196")]
+    public TreeNode CreateBinaryTree(int[][] descriptions)
+    {
+        var nodes = new Dictionary<int, TreeNode>();
+
+        for (int i = 0; i < descriptions.Length; i++)
+        {
+            var parentVal = descriptions[i][0];
+
+            if (!nodes.TryGetValue(parentVal, out TreeNode? parent))
+            {
+                parent = new(parentVal);
+                nodes[parentVal] = parent;
+            }
+
+            var childVal = descriptions[i][1];
+
+            if (!nodes.TryGetValue(childVal, out TreeNode? child))
+            {
+                child = new(childVal);
+                nodes[childVal] = child;
+            }
+
+            if (descriptions[i][2] == 1)
+                parent.left = child;
+            else
+                parent.right = child;
+        }
+
+        for (int i = 0; i < descriptions.Length; i++)
+            nodes.Remove(descriptions[i][1]);
+
+        return nodes.Single().Value;
+    }
 }
