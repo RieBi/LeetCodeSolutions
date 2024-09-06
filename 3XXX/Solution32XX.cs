875ca3c5613652aaa91499f6f2c029b8cd058d5b
RieBi <42877671+RieBi@users.noreply.github.com>
2024-09-06 16:09:53 +0200

    Add solution to problem 3217: Delete Nodes From Linked List Present in Array (dailiy challenge)

---
+++ 3XXX/Solution32XX.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Set3XXX;
+internal class Solution32XX
+{
+    [ProblemSolution("3217")]
+    public ListNode ModifiedList(int[] nums, ListNode head)
+    {
+        var set = nums.ToHashSet();
+
+        while (set.Contains(head.val))
+            head = head.next!;
+
+        var cur = head;
+        while (cur is not null)
+        {
+            while (cur.next is not null && set.Contains(cur.next.val))
+                cur.next = cur.next.next;
+
+            cur = cur.next;
+        }
+
+        return head;
+    }
+}
