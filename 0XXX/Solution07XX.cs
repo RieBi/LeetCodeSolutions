79d1ca582388b806f36fb9edc0cc522446a0cc0e
RieBi <42877671+RieBi@users.noreply.github.com>
2024-09-28 11:12:52 +0200

    Fix warnings

--- 0XXX/Solution07XX.cs
+++ 0XXX/Solution07XX.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace LeetCode.Set0XXX;
 internal class Solution07XX
 {
     [ProblemSolution("700")]
-    public TreeNode? SearchBST(TreeNode root, int val)
+    public TreeNode? SearchBST(TreeNode? root, int val)
     {
         while (root is not null)
         {
