166f79d289182498a2d4e02aee0edd1f3bd8a3da
RieBi <42877671+RieBi@users.noreply.github.com>
2024-10-28 19:21:22 +0100

    Add solution to problem 951: Flip Equivalent Binary Trees (daily challenge)

--- 0XXX/Solution09XX.cs
+++ 0XXX/Solution09XX.cs
@@ -258,6 +258,16 @@ internal class Solution09XX
         return result;
     }
 
+    [ProblemSolution("951")]
+    public bool FlipEquiv(TreeNode? root1, TreeNode? root2)
+    {
+        if (root1 is null || root2 is null)
+            return root1?.val == root2?.val;
+
+        return root1.val == root2.val &&
+            (FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right) || FlipEquiv(root1.left, root2.right) && FlipEquiv(root1.right, root2.left));
+    }
+
     [ProblemSolution("959")]
     public int RegionsBySlashes(string[] grid)
     {
