e79e1fff9924c962a31ab8534adc6bfb1617cc85
RieBi <42877671+RieBi@users.noreply.github.com>
2024-11-09 07:47:11 +0100

    Add solution to problem 3133: Minimum Array End (daily challenge)

---
+++ 3XXX/Solution31XX.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Set3XXX;
+internal class Solution31XX
+{
+    [ProblemSolution("3133")]
+    public long MinEnd(int n, int x)
+    {
+        n--;
+        long result = x;
+
+        var mask = 1L;
+        while (n > 0)
+        {
+            while ((result & mask) != 0)
+                mask <<= 1;
+
+            if (n % 2 == 1)
+                result |= mask;
+
+            n >>= 1;
+            mask <<= 1;
+        }
+
+        return result;
+    }
+}
