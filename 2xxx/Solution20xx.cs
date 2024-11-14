da043ec4bf6e3aae817c20cd88e90df6d8c2b2a7
RieBi <42877671+RieBi@users.noreply.github.com>
2024-11-14 10:47:09 +0100

    Add solution to problem 2064: Minimized Maximum of Products Distributed to Any Store (daily challenge)

--- 2xxx/Solution20xx.cs
+++ 2xxx/Solution20xx.cs
@@ -266,6 +266,36 @@ internal class Solution20XX
             return [minDist, lastInd - firstInd];
     }
 
+    [ProblemSolution("2064")]
+    public int MinimizedMaximum(int n, int[] quantities)
+    {
+        var left = (int)(quantities.Sum(f => (long)f) / n);
+        if (left == 0)
+            return 1;
+
+        var right = quantities.Max();
+
+        while (left <= right)
+        {
+            var mid = (left + right) / 2;
+
+            var count = 0;
+            for (int i = 0; i < quantities.Length; i++)
+            {
+                count += quantities[i] / mid;
+                if (quantities[i] % mid != 0)
+                    count++;
+            }
+
+            if (count <= n)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+
     [ProblemSolution("2070")]
     public int[] MaximumBeauty(int[][] items, int[] queries)
     {
