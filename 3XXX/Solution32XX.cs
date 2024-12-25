34cf0e3f69af2de77e2f7e794d8ba27e3c242d5b
RieBi <42877671+RieBi@users.noreply.github.com>
2024-12-25 17:37:15 +0100

    Add solution to problem 3203: Find Minimum Diameter After Merging Two Trees

--- 3XXX/Solution32XX.cs
+++ 3XXX/Solution32XX.cs
@@ -1,6 +1,107 @@
-﻿namespace LeetCode.Set3XXX;
+﻿using System.Numerics;
+
+namespace LeetCode.Set3XXX;
 internal class Solution32XX
 {
+    [ProblemSolution("3203")]
+    public int MinimumDiameterAfterMerge(int[][] edges1, int[][] edges2)
+    {
+        var (length1, diameter1) = getDist(edges1);
+        var (length2, diameter2) = getDist(edges2);
+
+        return Math.Max(length1 + length2 + 1, Math.Max(diameter1, diameter2));
+
+        (int length, int diameter) getDist(int[][] edges)
+        {
+            if (edges.Length <= 1)
+                return (edges.Length, edges.Length);
+
+            var graph = new List<int>[edges.Length + 1];
+
+            for (int i = 0; i < graph.Length; i++)
+                graph[i] = [];
+
+            for (int i = 0; i < edges.Length; i++)
+            {
+                var (a, b) = (edges[i][0], edges[i][1]);
+
+                graph[a].Add(b);
+                graph[b].Add(a);
+            }
+
+            var dist = new List<int>[edges.Length + 1];
+            for (int i = 0; i < dist.Length; i++)
+                dist[i] = new(capacity: graph[i].Count);
+
+            var stack = new Stack<DiameterStack>();
+            stack.Push(new(0, -1, 0));
+
+            while (stack.Count > 0)
+            {
+                var item = stack.Peek();
+
+                if (item.NextIndex == graph[item.Node].Count || item.Node != 0 && graph[item.Node].Count == 1)
+                {
+                    stack.Pop();
+                    if (item.Parent != -1)
+                    {
+                        var distList_ = dist[item.Parent];
+                        var list = graph[item.Parent];
+
+                        if (list[distList_.Count] != item.Node)
+                            distList_.Add(-1);
+
+                        var maxDist = dist[item.Node].Count == 0 ? 0 : dist[item.Node].Max();
+                        distList_.Add(maxDist + 1);
+                    }
+                }
+                else if (graph[item.Node][item.NextIndex] == item.Parent)
+                    item.NextIndex++;
+                else
+                {
+                    stack.Push(new(graph[item.Node][item.NextIndex], item.Node, 0));
+                    item.NextIndex++;
+                }
+            }
+
+            var cur = 0;
+            var prevMax = 0;
+
+            while (true)
+            {
+                var distList = dist[cur];
+
+                var maxInd = 0;
+                for (int i = 1; i < distList.Count; i++)
+                {
+                    if (distList[i] > distList[maxInd])
+                        maxInd = i;
+                }
+
+                var maxVal = distList[maxInd];
+
+                for (int i = 0; i < distList.Count; i++)
+                {
+                    if (i != maxInd && distList[i] > prevMax)
+                        prevMax = distList[i];
+                }
+
+                if (maxVal - prevMax < 2)
+                    return (Math.Max(maxVal, prevMax), maxVal + prevMax);
+
+                cur = graph[cur][maxInd];
+                prevMax++;
+            }
+        }
+    }
+
+    private sealed class DiameterStack(int node, int parent, int nextIndex)
+    {
+        public int Node = node;
+        public int Parent = parent;
+        public int NextIndex = nextIndex;
+    }
+
     [ProblemSolution("3217")]
     public ListNode ModifiedList(int[] nums, ListNode head)
     {
