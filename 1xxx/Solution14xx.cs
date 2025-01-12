9068f4f4351940f4f3a81e3ce25531f369fddbbb
RieBi <42877671+RieBi@users.noreply.github.com>
2025-01-12 18:52:18 +0100

    Add solution to problem 1408: String Matching in an Array

--- 1xxx/Solution14xx.cs
+++ 1xxx/Solution14xx.cs
@@ -71,7 +71,30 @@ internal class Solution14XX
         }
 
         return str.ToString();
-    }   
+    }
+
+    [ProblemSolution("1408")]
+    public IList<string> StringMatching(string[] words)
+    {
+        var result = new List<string>();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j < words.Length; j++)
+            {
+                if (i == j)
+                    continue;
+
+                if (words[j].Contains(words[i]))
+                {
+                    result.Add(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
 
     [ProblemSolution("1422")]
     public int MaxScore(string s)
