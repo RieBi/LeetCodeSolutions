c6c79d838177c994dad5fe3f7425825ca62351ab
Sviatoslav Zubar <riebisv@gmail.com>
2026-01-22 14:43:17 +0100

    Add the solution to problem 3314: Construct the Minimum Bitwise Array I

---
+++ 3XXX/Solution33XX.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Set3XXX;
+
+public class Solution33XX
+{
+    [ProblemSolution("3314")]
+    public int[] MinBitwiseArray(IList<int> nums)
+    {
+        var results = new int[nums.Count];
+        
+        for (var i = 0; i < nums.Count; i++)
+        {
+            var num = nums[i];
+
+            if (num == 2)
+            {
+                results[i] = -1;
+                continue;
+            }
+
+            var cur = 1;
+
+            while ((cur & num) > 0)
+                cur <<= 1;
+
+            var result = num ^ (cur >> 1);
+            results[i] = result;
+        }
+
+        return results;
+    }
+}
