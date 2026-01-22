57ad0eba20c6f1c974aa486957f3994905c74cb0
Sviatoslav Zubar <riebisv@gmail.com>
2026-01-22 15:02:29 +0100

    Add the solution to problem 3507: Minimum Pair Removal to Sort Array I

---
+++ 3XXX/Solution35XX.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Set3XXX;
+
+public class Solution35XX
+{
+    [ProblemSolution("3507")]
+    public int MinimumPairRemoval(int[] nums)
+    {
+        var list = nums.ToList();
+        var ops = 0;
+        
+        while (!isNonDecreasing())
+        {
+            ops++;
+            var min = 1;
+            var minValue = list[0] + list[1];
+
+            for (var i = 2; i < list.Count; i++)
+            {
+                if (list[i] + list[i - 1] < minValue)
+                {
+                    min = i;
+                    minValue = list[i] + list[i - 1];
+                }
+            }
+
+            list[min - 1] = minValue;
+            list.RemoveAt(min);
+        }
+
+        return ops;
+
+        bool isNonDecreasing()
+        {
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < list[i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
