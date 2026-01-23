e30c1e07473125ca77cc0ad2e7587d1f53f769e6
Sviatoslav Zubar <riebisv@gmail.com>
2026-01-23 17:08:55 +0100

    Add the solution to problem 3510: Minimum Pair Removal to Sort Array II

--- 3XXX/Solution35XX.cs
+++ 3XXX/Solution35XX.cs
@@ -1,9 +1,11 @@
-﻿namespace LeetCode.Set3XXX;
+﻿using System.Linq.Expressions;
+
+namespace LeetCode.Set3XXX;
 
 public class Solution35XX
 {
     [ProblemSolution("3507")]
-    public int MinimumPairRemoval(int[] nums)
+    public int MinimumPairRemoval1(int[] nums)
     {
         var list = nums.ToList();
         var ops = 0;
@@ -39,5 +41,91 @@ public class Solution35XX
 
             return true;
         }
+    }
+    
+    [ProblemSolution("3510")]
+    public int MinimumPairRemoval2(int[] nums)
+    {
+        var breaks = 0;
+        var set = new PriorityQueue<LinkedListNode<(long value, int position)>, (long sum, int position)>();
+        var linked = new LinkedList<(long value, int position)>();
+
+        linked.AddLast((nums[0], 0));
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < nums[i - 1])
+                breaks++;
+
+            var last = linked.AddLast((nums[i], i));
+
+            set.Enqueue(last, ((long)nums[i] + nums[i - 1], i));
+        }
+        
+        var ops = 0;
+        
+        while (breaks > 0)
+        {
+            ops++;
+
+            set.TryDequeue(out var element, out var priority);
+
+            while (element?.Previous is null || (element.Value.value + element.Previous.Value.value != priority.sum))
+                set.TryDequeue(out element, out priority);
+
+            var prevBreaks = 0;
+            var afterBreaks = 0;
+
+            var curNode = element;
+            var nextNode = curNode.Next;
+
+            var curNodeValue = curNode.Value.value;
+
+            var firstNode = curNode.Previous!;
+            var prevNode = firstNode.Previous;
+
+            var firstNodeValue = firstNode.Value.value;
+            if (curNodeValue < firstNodeValue)
+                prevBreaks++;
+            
+            var sum = firstNodeValue + curNodeValue;
+
+            var curNodePosition = curNode.Value.position;
+
+            if (prevNode is not null)
+            {
+                var prevNodeValue = prevNode.Value.value;
+                
+                set.Enqueue(curNode, (sum + prevNodeValue, curNodePosition));
+
+                if (firstNodeValue < prevNodeValue)
+                    prevBreaks++;
+
+                if (sum < prevNodeValue)
+                    afterBreaks++;
+            }
+
+            if (nextNode is not null)
+            {
+                var nextNodeValue = nextNode.Value.value;
+                var nextNodePosition = nextNode.Value.position;
+                
+                set.Enqueue(nextNode, (sum + nextNodeValue, nextNodePosition));
+
+                if (nextNodeValue < curNodeValue)
+                    prevBreaks++;
+
+                if (nextNodeValue < sum)
+                    afterBreaks++;
+            }
+
+            linked.Remove(firstNode);
+
+            curNode.Value = (sum, curNodePosition);
+
+            breaks += (afterBreaks - prevBreaks);
+        }
+
+        return ops;
     }
 }
