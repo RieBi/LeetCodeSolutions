f67dba40ac429fe20a5e473c234712c406932063
Sviatoslav Zubar <riebisv@gmail.com>
2026-02-04 01:03:24 +0100

    Add the solution to problem 3637: Trionic Array I

---
+++ 3XXX/Solution36XX.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Set3XXX;
+
+public class Solution36XX
+{
+    [ProblemSolution("3637")]
+    public bool IsTrionic(int[] nums)
+    {
+        if (nums[1] <= nums[0])
+            return false;
+
+        var changes = 0;
+        var dir = 1;
+        
+        for (var i = 2; i < nums.Length; i++)
+        {
+            var cur = nums[i] - nums[i - 1];
+            if (cur == 0)
+                return false;
+
+            if (Math.Sign(cur) != dir)
+            {
+                (changes, dir) = (changes + 1, -dir);
+                if (changes > 2)
+                    return false;
+            }
+        }
+
+        return changes == 2;
+    }
+}
