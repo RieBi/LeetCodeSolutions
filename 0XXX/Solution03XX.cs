22e28dab6915f8781b760c7a8eb6882586aa3636
Sviatoslav Zubar <riebisv@gmail.com>
2026-03-25 19:40:08 +0100

    Add the solution to problem 326: Power of Three

--- 0XXX/Solution03XX.cs
+++ 0XXX/Solution03XX.cs
@@ -25,6 +25,18 @@ internal class Solution03XX
 
         return dp.Count;
     }
+    
+    [ProblemSolution("326")]
+    public bool IsPowerOfThree(int n)
+    {
+        if (n < 1)
+            return false;
+
+        while (n % 3 == 0)
+            n /= 3;
+
+        return n == 1;
+    }
 
     [ProblemSolution("330")]
     public int MinPatches(int[] nums, int n)
