fc0f0a383d00b974115db5bcd5190b7f6d6572a2
Sviatoslav Zubar <riebisv@gmail.com>
2026-04-03 00:34:03 +0200

    Add the solution to problem 3418: Maximum Amount of Money Robot Can Earn

--- 3XXX/Solution34XX.cs
+++ 3XXX/Solution34XX.cs
@@ -1,6 +1,75 @@
 ﻿namespace LeetCode.Set3XXX;
 internal class Solution34XX
 {
+    [ProblemSolution("3418")]
+    public int MaximumAmount(int[][] coins)
+    {
+        var dp = new int[coins.Length][][];
+
+        for (var i = 0; i < coins.Length; i++)
+        {
+            dp[i] = new int[coins[i].Length][];
+
+            for (var j = 0; j < coins[i].Length; j++)
+                dp[i][j] = [-int.MaxValue, -int.MaxValue, -int.MaxValue];
+        }
+
+        var start = dp[0][0];
+
+        start[0] = coins[0][0];
+        start[1] = coins[0][0] < 0 ? 0 : start[0];
+        start[2] = start[1];
+
+        for (var i = 0; i < coins.Length; i++)
+        {
+            for (var j = 0; j < coins[i].Length; j++)
+            {
+                var arr = dp[i][j];
+                var (cur0, cur1, cur2) = (arr[0], arr[1], arr[2]);
+
+                if (i < coins.Length - 1)
+                {
+                    var val = coins[i + 1][j];
+                    var (d0, d1, d2) = (cur0 + val, cur1 + val, cur2 + val);
+
+                    if (val < 0)
+                    {
+                        d1 = Math.Max(d1, cur0);
+                        d2 = Math.Max(d2, cur1);
+                    }
+
+                    var newArr = dp[i + 1][j];
+
+                    newArr[0] = Math.Max(newArr[0], d0);
+                    newArr[1] = Math.Max(newArr[1], d1);
+                    newArr[2] = Math.Max(newArr[2], d2);
+                }
+
+                if (j < coins[i].Length - 1)
+                {
+                    var val = coins[i][j + 1];
+                    var (d0, d1, d2) = (cur0 + val, cur1 + val, cur2 + val);
+
+                    if (val < 0)
+                    {
+                        d1 = Math.Max(d1, cur0);
+                        d2 = Math.Max(d2, cur1);
+                    }
+
+                    var newArr = dp[i][j + 1];
+
+                    newArr[0] = Math.Max(newArr[0], d0);
+                    newArr[1] = Math.Max(newArr[1], d1);
+                    newArr[2] = Math.Max(newArr[2], d2);
+                }
+            }
+        }
+
+        var finalArr = dp[^1][^1];
+
+        return finalArr.Max();
+    }
+    
     [ProblemSolution("3474")]
     public string GenerateString(string str1, string str2)
     {
