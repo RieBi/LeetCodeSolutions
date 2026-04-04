dfb2a8064df11711f92159d2783be2cf1334e3cf
Sviatoslav Zubar <riebisv@gmail.com>
2026-04-05 00:12:27 +0200

    Add the solution to problem 3661: Maximum Walls Destroyed by Robots

--- 3XXX/Solution36XX.cs
+++ 3XXX/Solution36XX.cs
@@ -27,4 +27,92 @@ public class Solution36XX
 
         return changes == 2;
     }
+    
+    [ProblemSolution("3661")]
+    public int MaxWalls(int[] robots, int[] distance, int[] walls)
+    {
+        var sorted = new List<(bool isRobot, int pos, int distance, bool isMarked)>(capacity: robots.Length + walls.Length);
+
+        var same = 0;
+        var positions = new HashSet<int>(capacity: robots.Length);
+
+        for (var i = 0; i < robots.Length; i++)
+        {
+            sorted.Add((isRobot: true, pos: robots[i], distance: distance[i], isMarked: false));
+            positions.Add(robots[i]);
+        }
+
+        for (var i = 0; i < walls.Length; i++)
+        {
+            if (positions.Contains(walls[i]))
+                same++;
+            else
+                sorted.Add((isRobot: false, pos: walls[i], distance: 0, isMarked: false));
+        }
+
+        sorted.Sort((a, b) => a.pos.CompareTo(b.pos));
+
+        var ind = 0;
+
+        while (ind < sorted.Count)
+        {
+            while (ind < sorted.Count && !sorted[ind].isRobot)
+                ind++;
+
+            if (ind == sorted.Count)
+                break;
+
+            var cover = sorted[ind].pos + sorted[ind].distance;
+
+            ind++;
+
+            while (ind < sorted.Count && !sorted[ind].isRobot && sorted[ind].pos <= cover)
+            {
+                var cur = sorted[ind];
+
+                sorted[ind] = (false, cur.pos, 0, true);
+                ind++;
+            }
+        }
+
+        ind = 0;
+
+        var prevLeft = 0;
+        var prevRight = 0;
+
+        while (ind < sorted.Count)
+        {
+            while (ind < sorted.Count && !sorted[ind].isRobot)
+                ind++;
+
+            if (ind == sorted.Count)
+                break;
+            
+            var coverLeft =  sorted[ind].pos - sorted[ind].distance;
+
+            var leftCount = 0;
+            var leftFull = 0;
+            var rightCount = 0;
+
+            int j;
+            for (j = ind - 1; j >= 0 && !sorted[j].isRobot && !sorted[j].isMarked &&
+                                  sorted[j].pos >= coverLeft; j--)
+                leftCount++;
+
+            for (; j >= 0 && !sorted[j].isRobot && sorted[j].pos >= coverLeft; j--)
+                leftFull++;
+
+            for (j = ind + 1; j < sorted.Count && !sorted[j].isRobot && sorted[j].isMarked; j++)
+                rightCount++;
+
+            var curLeft = Math.Max(prevLeft + leftCount + leftFull, prevRight + leftCount);
+            var curRight = Math.Max(prevLeft, prevRight) + rightCount;
+
+            (prevLeft, prevRight) = (curLeft, curRight);
+
+            ind++;
+        }
+        
+        return Math.Max(prevLeft, prevRight) + same;
+    }
 }
